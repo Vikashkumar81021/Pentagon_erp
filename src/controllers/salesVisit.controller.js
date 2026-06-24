d8d5b30f6ceb0591c3e54d9ea8dcf27922ea2365
Vikashkumar81021 <viku81021@gmail.com>
2026-06-24 18:00:18 +0530

    add

--- src/controllers/salesVisit.controller.js
+++ src/controllers/salesVisit.controller.js
@@ -1,22 +1,24 @@
-import {asyncHandler} from '../utils/asyncHandler.js';
+import { asyncHandler } from "../utils/asyncHandler.js";
 import { STATUS_CODE } from "../constants/status.code.js";
 import { salesVisitValidator } from "../validators/salesVisit.validator.js";
-import { getSalesVisitsService, salesVisitService } from "../services/salesVisit.service.js";
-import { BadRequestError } from "../utils/error.js";
-
+import {
+  getSalesVisitsService,
+  salesVisitService,
+  updateSalesVisit,
+} from "../services/salesVisit.service.js";
 
 const createSalesVisitController = asyncHandler(async (req, res, next) => {
   const validateData = salesVisitValidator.parse(req.body);
   const salesVisit = await salesVisitService(validateData);
-    return res.status(STATUS_CODE.CREATED).json({
-        success: true,
-        message: "Sales Visit created successfully",
-        data: salesVisit,
-    });
+  return res.status(STATUS_CODE.CREATED).json({
+    success: true,
+    message: "Sales Visit created successfully",
+    data: salesVisit,
+  });
 });
 const getSalesVisitsController = asyncHandler(async (req, res, next) => {
   const salesVisits = await getSalesVisitsService();
-  return res.status(STATUS_CODE.OK).json({
+  return res.status(STATUS_CODE.SUCCESS).json({
     success: true,
     message: "Sales Visits fetched successfully",
     data: salesVisits,
@@ -24,12 +26,16 @@ const getSalesVisitsController = asyncHandler(async (req, res, next) => {
 });
 const updateSalesVisitController = asyncHandler(async (req, res, next) => {
   const { id } = req.params;
-    const validateData = salesVisitValidator.parse(req.body);
-    const salesVisit = await updateSalesVisit(id, validateData);
-    return res.status(STATUS_CODE.OK).json({
-        success: true,
-        message: "Sales Visit updated successfully",
-        data: salesVisit,
-    });
+  const validateData = salesVisitValidator.parse(req.body);
+  const salesVisit = await updateSalesVisit(id, validateData);
+  return res.status(STATUS_CODE.OK).json({
+    success: true,
+    message: "Sales Visit updated successfully",
+    data: salesVisit,
+  });
 });
-export { createSalesVisitController, getSalesVisitsController, updateSalesVisitController };
+export {
+  createSalesVisitController,
+  getSalesVisitsController,
+  updateSalesVisitController,
+};
