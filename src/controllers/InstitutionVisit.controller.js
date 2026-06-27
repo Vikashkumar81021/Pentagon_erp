7c858a88989db211760bad9a25ba3090fabbebe9
Shobhit rajput <shobhitrajput7774@gmail.com>
2026-06-27 16:39:47 +0530

    institutionVisitupdate

--- src/controllers/InstitutionVisit.controller.js
+++ src/controllers/InstitutionVisit.controller.js
@@ -4,6 +4,8 @@ import { STATUS_CODE } from "../constants/status.code.js";
 import {
   createInstitutionVisit,
   getInstitutionVisits,
+  updateInstitutionVisit,
+  deleteInstitutionVisit,
 } from "../services/InstitutionVisit.service.js";
 
 const createInstitutionVisitController = asyncHandler(async (req, res) => {
@@ -26,4 +28,32 @@ const getInstitutionVisitsController = asyncHandler(async (req, res) => {
   });
 });
 
-export {createInstitutionVisitController, getInstitutionVisitsController};
+const updateInstitutionVisitController = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+
+  const institutionVisit = await updateInstitutionVisit(id, req.body);
+
+  return res.status(STATUS_CODE.SUCCESS).json({
+    success: true,
+    message: "Institution Visit updated successfully",
+    data: institutionVisit,
+  });
+});
+
+const deleteInstitutionVisitController = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+
+  await deleteInstitutionVisit(id);
+
+  return res.status(STATUS_CODE.SUCCESS).json({
+    success: true,
+    message: "Institution Visit deleted successfully",
+  });
+});
+
+export {
+    createInstitutionVisitController, 
+    getInstitutionVisitsController, 
+    updateInstitutionVisitController,
+    deleteInstitutionVisitController,
+};
--- src/services/InstitutionVisit.service.js
+++ src/services/InstitutionVisit.service.js
@@ -17,4 +17,31 @@ const getInstitutionVisits = async () => {
   });
 };
 
-export {createInstitutionVisit, getInstitutionVisits};
+const updateInstitutionVisit = async (id, data) => {
+  return await prisma.institutionVisit.update({
+    where: {
+      id: Number(id),
+    },
+    data: {
+      ...data,
+      planned_visit_date: data.planned_visit_date
+        ? new Date(data.planned_visit_date)
+        : undefined,
+    },
+  });
+};
+
+const deleteInstitutionVisit = async (id) => {
+  return await prisma.institutionVisit.delete({
+    where: {
+      id: Number(id),
+    },
+  });
+};
+
+export {
+    createInstitutionVisit, 
+    getInstitutionVisits, 
+    updateInstitutionVisit, 
+    deleteInstitutionVisit
+};
