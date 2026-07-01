5309bc8e5479c8786a6032fe6fc4d9655bd57f7d
Shobhit rajput <shobhitrajput7774@gmail.com>
2026-07-01 13:48:56 +0530

    notice filter api

--- src/routes/notice.route.js
+++ src/routes/notice.route.js
@@ -3,7 +3,8 @@ import authMiddleware from '../middleware/auth.middleware.js';
 import {
      createNoticeController,
       getNoticesController,
-      deleteNoticeController
+      deleteNoticeController,
+      getNoticeByTypeController
 }from '../controllers/notice.controllers.js';
 
 const router = express.Router();
@@ -12,4 +13,5 @@ router.get("/notices", getNoticesController);
 
 router.post("/notice/createNotice", createNoticeController);
 router.delete("/notice/:id", deleteNoticeController);
+router.get("/notice/filter", getNoticeByTypeController);
 export default router;
