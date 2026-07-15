ab452f5489137b75072a9081d234ea6cd22914ef
Vikashkumar81021 <viku81021@gmail.com>
2026-07-15 14:20:28 +0530

    modify job application db

--- prisma/migrations/20260715084916_update_jobtype_enum/migration.sql
+++ prisma/migrations/20260715084916_update_jobtype_enum/migration.sql
@@ -16,6 +16,24 @@ DROP COLUMN "job_status",
 DROP COLUMN "job_type",
 DROP COLUMN "roles_requirements",
 ADD COLUMN     "description" TEXT NOT NULL,
-ADD COLUMN     "employmentType" "JobType" NOT NULL,
+ADD COLUMN     "employmentType" TEXT NOT NULL,
 ADD COLUMN     "jobStatus" TEXT NOT NULL DEFAULT 'OPEN',
 ADD COLUMN     "jobTitle" TEXT NOT NULL;
+
+-- DropEnum
+DROP TYPE "JobType";
+
+-- CreateTable
+CREATE TABLE "JobApplication" (
+    "id" SERIAL NOT NULL,
+    "hiringRequirementId" INTEGER NOT NULL,
+    "candidateName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "mobile" TEXT NOT NULL,
+    "appliedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "JobApplication_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "JobApplication" ADD CONSTRAINT "JobApplication_hiringRequirementId_fkey" FOREIGN KEY ("hiringRequirementId") REFERENCES "HiringRequirement"("id") ON DELETE CASCADE ON UPDATE CASCADE;
