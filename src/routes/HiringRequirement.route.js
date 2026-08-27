1d69ee0296abf22741653a97ee2b7efdd8b53544
Vikashkumar81021 <viku81021@gmail.com>
2026-08-27 12:42:12 +0530

    add job application

--- src/routes/HiringRequirement.route.js
+++ src/routes/HiringRequirement.route.js
@@ -35,38 +35,8 @@ import {
 
 const router = express.Router();
 
-/**
- * @swagger
- * /hiring/create:
- *   post:
- *     summary: Create Hiring Requirement
- *     tags:
- *       - Hiring Requirement
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/HiringRequirementCreate'
- *     responses:
- *       201:
- *         description: Hiring requirement created successfully
- *       400:
- *         description: Validation error
- */
 router.post("/hiring/create", createHiringRequirementController);
 
-/**
- * @swagger
- * /fetchhiring:
- *   get:
- *     summary: Get All Hiring Requirements
- *     tags:
- *       - Hiring Requirement
- *     responses:
- *       200:
- *         description: Hiring requirements fetched successfully
- */
 router.get("/fetchhiring", getHiringRequirementController);
 router.get("/hiringId/:id", getHiringRequirementByIdController);
 router.put("/hiring/:id", updateHiringRequirementController);
