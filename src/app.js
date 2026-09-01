2eaa5e111f59af06478c0e77845f9f69dfe48216
Vikashkumar81021 <viku81021@gmail.com>
2026-09-01 12:52:08 +0530

    integrate s3

--- src/app.js
+++ src/app.js
@@ -51,7 +51,7 @@ app.use(
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
-  })
+  }),
 );
 app.use(
   helmet({
